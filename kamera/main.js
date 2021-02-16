9976b854f5ce4f1114d0e96686d28af7baa123c4
Isson96 <37865962+Isson96@users.noreply.github.com>
2021-02-16 08:29:36 +0100

    Alpine

--- kamera/main.js
+++ kamera/main.js
@@ -1,6 +1,19 @@
+var state = {
+    story: {
+        loaded: false,
+        data: {}
+    },
+    user: {
+        showQRScanner: false
+    },
+    showQRScanner: function () {
+        cameraStart();
+        this.user.showQRScanner = true;
+    }
+};
 
-
-
+// Access the device camera and stream to cameraView
+function cameraStart() {
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "environment" }, audio: false };
 // Define constants
@@ -8,8 +21,15 @@ const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
@@ -21,28 +41,11 @@ function cameraStart() {
     });
 }
 
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-
-
 function showWindow() {
-
     if (document.getElementById("camera--output").style.visibility === "visible") {
         alert("CLICKI-TAKE-A-PIC!");
     }
   
     document.getElementById("camera--output").style.visibility = "visible";
 }
-
-
-
-
-
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+window.state = state;
