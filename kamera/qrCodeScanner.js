8635f034cc8d64037d6cf15197ecffb2ebefebd8
Tomie Lee <jwwlee@gmail.com>
2021-02-23 09:30:24 +0100

    Bugfixes

--- kamera/qrCodeScanner.js
+++ kamera/qrCodeScanner.js
@@ -20,26 +20,25 @@ qrcode2.callback = res => {
         canvasElement.hidden = true;
         btnScanQR.hidden = false;
 
+        url =  "kamera/ljudfiler/"+res+".mp4",
 
         $.ajax({    // ajax hade en bra funktion f�r att kolla om en fil finns i en mapp  
-            url: "/ljudfiler/"+res+".mp4",
-            type: 'HEAD',
+            type: 'HEAD', 
             error: function () { // ingen fil hittades med QR-texten, den har skannat fel
                 audioDiv.innerHTML = "";
-                alert("error");
+                alert("error " + res);
             },
             success: function () {  // tjoho! Pumpa ut html-kod med audiofilen och en playknapp
-				 alert("Fungerar");
-                audioDiv.innerHTML ="<div class='audioWrapper'>" +
-                                        "<audio controls id = 'myAudio' > " +
-                                            "<source src = '/ljudfiler/"+res+".mp4' type = 'audio/wav'>" +
-                                        "</audio >" +
-                                        "<button class='playBtn' onclick = 'playAudio()'> Play Audio </button>" +
-                                    "</div>";
-                                   
-                                  
+            alert("Fungerar" + res + " och url: " + url);
+                    audioDiv.innerHTML ="<div class='audioWrapper'>" +
+                                            "<audio controls id = 'myAudio' > " +
+                                                "<source src = "+ url + "type = 'audio/wav'>" +
+                                                "<p>Playing "+ url + "<p>"
+                                            "</audio >" +
+                                            "<button class='playBtn' onclick = 'playAudio()'> Play Audio </button>" +
+                                        "</div>";                    
             }
-        });
+        })
   }
 
 };
